6e6f9864584dd3f1e095253676a0b78477d13078
Margherita88 <lilac.spring2403@gmail.com>
2025-08-13 23:32:24 +0900

    ボタンの追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 export const App = () => {
+    // ボタンを押した時に実行する関数を定義
+    const onClickButton = () => {
+        alert();
+    };
     return (
         <>
             <h1>こんにちは！</h1>
             <p>お元気ですか</p>
+            {/* 波括弧を使えば、HTMLの中にJavaScript を書ける */}
+            <button onClick={onClickButton}>ボタン</button>
         </>
     );
 };
